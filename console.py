d4c19e0dbe41ddaad44ada43bc0e3190a0aebb53
josh <josh.pontes99@gmail.com>
2022-03-30 21:38:38 -0700

    moved cmd2_plugins into src folder

--- console.py
+++ console.py
@@ -5,18 +5,18 @@ from rich.console import Console
 import json
 
 #import plugin files here
-import cmd2_plugins.clients_plugin
-import cmd2_plugins.connections_plugin
-import cmd2_plugins.generate_plugin
-import cmd2_plugins.commands_plugin
+import src.cmd2_plugins.clients_plugin
+import src.cmd2_plugins.connections_plugin
+import src.cmd2_plugins.generate_plugin
+import src.cmd2_plugins.commands_plugin
 
 
 class App(
         #We need to install the plugins here
-        cmd2_plugins.clients_plugin.Plugin,
-        cmd2_plugins.connections_plugin.Plugin, 
-        cmd2_plugins.generate_plugin.Plugin,
-        cmd2_plugins.commands_plugin.Plugin,
+        src.cmd2_plugins.clients_plugin.Plugin,
+        src.cmd2_plugins.connections_plugin.Plugin, 
+        src.cmd2_plugins.generate_plugin.Plugin,
+        src.cmd2_plugins.commands_plugin.Plugin,
         Cmd):
 
     console = Console()
